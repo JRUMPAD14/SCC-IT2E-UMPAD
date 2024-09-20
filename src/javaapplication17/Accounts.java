c3fa5535abc3b697ced13afa3b7b43e2095162bd
JRUMPAD14 <johnumpad316@gmail.com>
2024-09-20 22:33:31 +0800

    UNTA PASAR

---
+++ src/javaapplication17/Accounts.java
@@ -0,0 +1,17 @@
+
+package javaapplication17;
+
+
+public class Accounts {
+
+    void addAccounts(int ID, String fName, String lName, String Eadd, String usern, String passw) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    void viewAccounts() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
